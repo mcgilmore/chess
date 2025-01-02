456157f787fbff9618b0dbfe466b3a6232c8240d
mcgilmore <michael_gilmore@live.com>
2025-01-02 11:31:39 +0000

    small graphical improvements

--- src/pieces.rs
+++ src/pieces.rs
@@ -8,7 +8,6 @@ impl Pieces {
         Pieces
     }
 
-    // Placeholder: pieces made of simple shapes (circles, polygons, etc.)
     pub fn draw_piece(
         &self,
         ctx: &mut Context,
@@ -18,19 +17,14 @@ impl Pieces {
         x: f32,
         y: f32,
     ) -> GameResult<()> {
-        let (fill_color, outline_color) = match piece_color {
-            crate::PieceColor::White => (Color::from_rgb(220, 220, 220), Color::BLACK),
-            crate::PieceColor::Black => (Color::from_rgb(50, 50, 50), Color::WHITE),
-        };
-
         let mut mb = MeshBuilder::new();
         // Scaling factors based on TILE_SIZE
         let tile_size = crate::TILE_SIZE;
-        let grid_square = tile_size / 10.0; // Each square will be a 10x10 grid
-        let piece_radius = tile_size * 0.4;
+        let grid_square = tile_size / 10.0;
         let piece_x_offset = tile_size * 0.2;
+        let piece_y_offset = tile_size * 0.1;
         let piece_color = match piece_color {
-            crate::PieceColor::White => Color::from_rgb(240, 240, 240),
+            crate::PieceColor::White => Color::from_rgb(230, 230, 230),
             crate::PieceColor::Black => Color::from_rgb(50, 50, 50),
         };
         
@@ -42,7 +36,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 2.0,
-                        y + grid_square * 2.0,
+                        y + piece_y_offset + grid_square * 2.0,
                         grid_square * 2.0, // width
                         grid_square * 6.0, // height 
                     ),
@@ -52,7 +46,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square,
-                        y + grid_square * 3.0,
+                        y + piece_y_offset + grid_square * 3.0,
                         grid_square * 4.0, // width
                         grid_square * 2.0, // height 
                     ),
@@ -62,7 +56,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 1.5,
-                        y + grid_square * 2.5,
+                        y + piece_y_offset + grid_square * 2.5,
                         grid_square * 3.0, // width
                         grid_square * 3.0, // height 
                     ),
@@ -73,7 +67,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset,
-                        y + grid_square * 7.0,
+                        y + piece_y_offset + grid_square * 7.0,
                         grid_square * 6.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -83,7 +77,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square,
-                        y + grid_square * 6.5,
+                        y + piece_y_offset + grid_square * 6.5,
                         grid_square * 4.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -95,7 +89,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 2.0,
-                        y + grid_square * 2.0,
+                        y + piece_y_offset + grid_square * 2.0,
                         grid_square * 2.0, // width
                         grid_square * 5.0, // height 
                     ),
@@ -105,7 +99,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 2.0,
-                        y + grid_square * 2.5,
+                        y + piece_y_offset + grid_square * 2.5,
                         grid_square * 3.5, // width
                         grid_square * 2.0, // height 
                     ),
@@ -115,7 +109,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 2.0,
-                        y + grid_square * 1.5,
+                        y + piece_y_offset + grid_square * 1.5,
                         grid_square * 0.5, // width
                         grid_square * 0.5, // height 
                     ),
@@ -126,7 +120,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset,
-                        y + grid_square * 7.0,
+                        y + piece_y_offset + grid_square * 7.0,
                         grid_square * 6.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -136,7 +130,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square,
-                        y + grid_square * 6.5,
+                        y + piece_y_offset + grid_square * 6.5,
                         grid_square * 4.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -149,7 +143,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 2.0,
-                        y + grid_square * 2.0,
+                        y + piece_y_offset + grid_square * 2.0,
                         grid_square * 2.0, // width
                         grid_square * 5.0, // height 
                     ),
@@ -159,7 +153,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 3.0,
-                        y + grid_square,
+                        y + piece_y_offset + grid_square,
                         grid_square * 0.5, // width
                         grid_square * 1.0, // height 
                     ),
@@ -169,7 +163,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 2.5,
-                        y + grid_square * 1.5,
+                        y + piece_y_offset + grid_square * 1.5,
                         grid_square * 1.5, // width
                         grid_square * 1.0, // height 
                     ),
@@ -179,7 +173,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square,
-                        y + grid_square * 2.5,
+                        y + piece_y_offset + grid_square * 2.5,
                         grid_square * 4.0, // width
                         grid_square * 0.5, // height 
                     ),
@@ -190,7 +184,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset,
-                        y + grid_square * 7.0,
+                        y + piece_y_offset + grid_square * 7.0,
                         grid_square * 6.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -200,7 +194,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square,
-                        y + grid_square * 6.5,
+                        y + piece_y_offset + grid_square * 6.5,
                         grid_square * 4.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -213,7 +207,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 1.5,
-                        y + grid_square * 2.0,
+                        y + piece_y_offset + grid_square * 2.0,
                         grid_square * 3.0, // width
                         grid_square * 6.0, // height 
                     ),
@@ -223,7 +217,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 1.0,
-                        y + grid_square * 1.0,
+                        y + piece_y_offset + grid_square * 1.0,
                         grid_square * 1.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -233,7 +227,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 2.5,
-                        y + grid_square * 1.0,
+                        y + piece_y_offset + grid_square * 1.0,
                         grid_square * 1.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -243,7 +237,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square * 4.0,
-                        y + grid_square * 1.0,
+                        y + piece_y_offset + grid_square * 1.0,
                         grid_square * 1.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -254,7 +248,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset,
-                        y + grid_square * 7.0,
+                        y + piece_y_offset + grid_square * 7.0,
                         grid_square * 6.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -264,7 +258,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square,
-                        y + grid_square * 6.5,
+                        y + piece_y_offset + grid_square * 6.5,
                         grid_square * 4.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -278,7 +272,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset,
-                        y + grid_square * 7.0,
+                        y + piece_y_offset + grid_square * 7.0,
                         grid_square * 6.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -288,7 +282,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square,
-                        y + grid_square * 6.5,
+                        y + piece_y_offset + grid_square * 6.5,
                         grid_square * 4.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -302,7 +296,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset,
-                        y + grid_square * 7.0,
+                        y + piece_y_offset + grid_square * 7.0,
                         grid_square * 6.0, // width
                         grid_square * 1.0, // height 
                     ),
@@ -312,7 +306,7 @@ impl Pieces {
                     DrawMode::fill(),
                     ggez::graphics::Rect::new(
                         x + piece_x_offset + grid_square,
-                        y + grid_square * 6.5,
+                        y + piece_y_offset + grid_square * 6.5,
                         grid_square * 4.0, // width
                         grid_square * 1.0, // height 
                     ),
